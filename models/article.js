31c3e6ecbd9b805936705e3de26d34c1e5a50f3e
Carolyn Vasisko <carolyn@mothermedia.com>
2018-06-22 00:03:16 -0500

    server and routes good, working on scraping/db

--- models/article.js
+++ models/article.js
@@ -4,8 +4,7 @@ const Schema = mongoose.Schema;
 const articleSchema = new Schema({
   title: { type: String, required: true },
   url: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now }
+  date: { type: Date, default: Date.now },
 });
 
 const Article = mongoose.model("Article", articleSchema);
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 3001;
 const app = express();
 const routes = require("./routes");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
